1895f73e7fd9861031e6824e190a8dd26bd26dc6
Chijeong In <cde0409@naver.com>
2021-05-29 23:20:27 +0900

    프로젝트 페이지 카드 추가

--- src/views/pages/project/Project.js
+++ src/views/pages/project/Project.js
@@ -1,11 +1,78 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import {
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CRow,
+} from  '@coreui/react'
+import axios from 'axios';
 
 const Project = (prop) => {
-  console.log('project ', prop);
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/v1/projects')
+      .then(res => {
+        console.log(res);
+        this.setState({
+          message: res.date.message
+        })
+      })
+      .catch(res => console.log(res));
+  });
+
   return (
-    <div>
-      <p>project</p>
-    </div>
+    <>
+      <CRow>
+        <CCol xs="12" sm="6" md="4" lg="3" xl="3">
+          <CCard>
+            <CCardHeader>
+              Card title
+            </CCardHeader>
+            <CCardBody>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+              laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+              ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+            </CCardBody>
+          </CCard>
+        </CCol>
+        <CCol xs="12" sm="6" md="4" xl="3">
+          <CCard>
+            <CCardHeader>
+              Card title
+            </CCardHeader>
+            <CCardBody>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+              laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+              ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+            </CCardBody>
+          </CCard>
+        </CCol>
+        <CCol xs="12" sm="6" md="4" xl="3">
+          <CCard>
+            <CCardHeader>
+              Card title
+            </CCardHeader>
+            <CCardBody>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+              laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+              ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+            </CCardBody>
+          </CCard>
+        </CCol>
+        <CCol xs="12" sm="6" md="4" xl="3">
+          <CCard>
+            <CCardHeader>
+              Card title
+            </CCardHeader>
+            <CCardBody>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut
+              laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
+              ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
   )
 }
 
